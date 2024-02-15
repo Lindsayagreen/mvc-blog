95fada2a7defb5ad4fd4f8e828defc0219e6a971
lindsayagreen <lindsgree@gmail.com>
2024-02-15 10:37:53 -0600

    final

---
+++ public/js/login.js
@@ -0,0 +1,55 @@
+// Login form handler
+const signInFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the sign-in form
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// Signup form handler
+const signUpFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the sign-up form
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// Add event listeners to the forms
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", signInFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signUpFormHandler);
